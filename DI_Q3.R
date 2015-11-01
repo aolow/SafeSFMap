cc99996e9c7e514b2f64de9da24165c6fc428785
Aleksandra <aolow@users.noreply.github.com>
2015-11-01 12:00:48 -0800

    annotation clean-up

--- DI_Q3.R
+++ DI_Q3.R
@@ -1,4 +1,4 @@
-# SafeSFMap Proposal  - Exploratory Analysis and Visualization
+# SafeSF Path Finder Proposal  - Exploratory Analysis and Visualization
 # by A. Olow
 
 # load libraries
@@ -9,16 +9,13 @@ library(ggplot2)
 library(ggmap)
 library(gridExtra)
 
-# # load dataset
-# json_file <- "https://data.sfgov.org/resource/gxxq-x39z.json"
-# data <- fromJSON(json_file)
+### Load dataset
+json_file <- "https://data.sfgov.org/resource/gxxq-x39z.json"
+crime <- fromJSON(json_file)
 
-# Load crime data
-crime <- read.csv("/Users/aolow/Box\ Sync/Documents/R/DataIncubator/Crime/crime.csv")
+### Clean data 
 
-# Clean data 
-
-# subset for crimes of violence alone - directly contributing to street safety for a passer-by
+# Subset for crimes of violence alone - directly contributing to street safety for a passer-by
 pedestrian_unsafe <- c("ASSAULT", "DISORDERLY CONDUCT", "DRIVING UNDER THE INFLUENCE", 
                        "DRUG/NARCOTIC", "DRUNKENNESS", "LARCENY/THEFT", "LOITERING", "ROBBERY", "SEX OFFENSES, FORCIBLE",
                        "STOLEN PROPERTY")
@@ -43,20 +40,17 @@ crime2$Month <- format(crime2$Date2, '%m')
 # 2015 incomplete >> remove
 crime2 <- crime2[!crime2$Year==2015,]
 
-###########
-# Visualize
+### Visualize Data
 
 # Get map of SF from Google Maps
 SF <- get_map(location = c(lon = -122.4437, lat = 37.76), zoom = 12, maptype = 'roadmap')
 SF_MAP = ggmap(SF) + 
   scale_x_continuous(limits = c(-122.52, -122.35), expand = c(0, 0)) +
   scale_y_continuous(limits = c(37.7, 37.82), expand = c(0, 0))
-#SF_MAP
 
-### FIGURE 1
+## FIGURE 1
 
-# TOP PANEL - Increase in crime over time per districts
-
+# Increase in crime over time per districts
 annual <- aggregate(crime2$IncidntNum~crime2$Category*crime2$Year, data=crime2, function(x) length(x))
 names(annual) <- c("Category", "Year", "IncidntNum")
 
@@ -68,8 +62,7 @@ p_annual <- ggplot(annual, aes(x = Year, y = IncidntNum, fill=Category)) +
   geom_text(aes(y = (IncidntNum + 15), label = IncidntNum, angle=45), col='grey30', size=3.5, position = position_dodge(width=1))+
   labs(x="", y="Average Number of Incidents", title="Incidence of San Francisco Crimes per Year")
 
-
-# simple prediction total pedestrian-endangering crimes per neighborhood in 2015
+# Simple prediction total pedestrian-endangering crimes per neighborhood for 2015
 annual2 <- aggregate(crime2$IncidntNum~crime2$PdDistrict*crime2$Year, data=crime2, function(x) length(x))
 names(annual2) <- c("PdDistrict", "Year", "IncidntNum")
 
@@ -95,24 +88,22 @@ p2015 <- ggplot(preds, aes(x = PdDistrict, y = fit)) +
         plot.title = element_text(face='bold', size=18))+
   labs(x="", y="Predicted Number of Incidents 2015", title="Projected Pedestrian-Endangering Incidents 2015")
 
-
-# MAP
+# 2014 SF crime MAP
 
 map2014 <- SF_MAP + geom_point(data = crime2[crime2$Year=="2014",], aes(x = X, y = Y, color = Category), 
                                size=2, alpha = 0.8) + scale_colour_brewer(palette="Set1", guide=F) + theme_bw()+
   labs(x="", y="", title="San Francisco Pedestrian Endangering Crimes 2014")+ 
   theme(plot.title = element_text(face='bold', size=18), axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks = element_blank())
 
+
+# Arrange Panels for Figure 1
 Fig1 <- grid.arrange(p_annual, arrangeGrob(map2014, p2015, ncol = 2), nrow=2, heights=c(1,1))
 
+## FIGURE 2
 
-
-#### FIGURE 2
-
-# TOP PANEL 
+# TOP PANEL - Day/time of the week crime distribution
 daytime <- aggregate(crime2$IncidntNum~crime2$Category*crime2$DayOfWeek*crime2$TimeOfDay, data=crime2, function(x) length(x))
 names(daytime) <- c("Category", "DayOfWeek", "TimeOfDay", "IncidntNum")
-
 
 p_week <- ggplot(daytime, aes(x = DayOfWeek, y = IncidntNum, fill=Category)) + 
   geom_bar(stat = "identity")+ theme_bw() +
@@ -125,10 +116,7 @@ p_week <- ggplot(daytime, aes(x = DayOfWeek, y = IncidntNum, fill=Category)) +
   labs(x="", y="Average Number of Incidents / Year", 
        title="Daytime Distribution of San Francisco Crimes [Avg / Year] \n")
 
-# BOTTOM PANEL
-
-####
-# total crimes per type per district
+# MIDDLE PANEL - total crimes per type per district
 by_district <- aggregate(crime2$IncidntNum~crime2$Category*crime2$PdDistrict*crime2$TimeOfDay, data=crime2, function(x) length(x))
 names(by_district) <- c("Category", "PdDistrict", "TimeOfDay", "IncidntNum")
 
@@ -143,8 +131,7 @@ p_by_district <- ggplot(by_district, aes(x = PdDistrict, y = IncidntNum, fill=Ca
   coord_flip() +
   labs(x="", y="Average Number of Incidents / Year")
 
-
-## Add a panel of daytime maps
+## BOTTOM PANEL - daytime maps
 
 morning <- SF_MAP + geom_point(data = crime2[crime2$TimeOfDay=="morning",], aes(x = X, y = Y, color = Category), 
                                size=1.5, alpha = 0.5) + scale_colour_brewer(palette="Set1", guide=FALSE) + theme_bw()+
